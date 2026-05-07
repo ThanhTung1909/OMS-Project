dabb195b4c3d17e6aba30ed5aab669baef62eeb3
ThanhTungstudent <phamthanhtung200698@gmail.com>
2026-05-07 07:57:29 +0700

    pass: notification

---
+++ backend/notification-service/src/main/java/com/oms/notificationservice/client/ProfileClient.java
@@ -0,0 +1,13 @@
+package com.oms.notificationservice.client;
+
+import com.oms.notificationservice.dto.CustomerProfileResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "profile-service", url = "${app.services.profile-service.url:http://profile-service:8088}")
+public interface ProfileClient {
+
+    @GetMapping("/api/v1/customers/{id}")
+    CustomerProfileResponse getCustomerById(@PathVariable("id") String id);
+}

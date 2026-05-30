1cfeda5926cd3c80182a284bd237703958eba211
ThanhTungstudent <phamthanhtung200698@gmail.com>
2026-05-30 21:36:36 +0700

    api lay danh sach san pham

--- backend/product-service/src/main/java/com/oms/productservice/service/CategoryService.java
+++ backend/product-service/src/main/java/com/oms/productservice/service/CategoryService.java
@@ -1,7 +1,24 @@
 package com.oms.productservice.service;
 
+import com.oms.productservice.dto.categoryDTO.CategoryResponse;
+import com.oms.productservice.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public List<CategoryResponse> getAllCategories() {
+        return categoryRepository.findAll().stream()
+                .map(category -> CategoryResponse.builder()
+                        .id(category.getId())
+                        .name(category.getName())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
